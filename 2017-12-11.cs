30b03ea00f75c4ac817263c8d2ec4405cbbf2cef
Eric Friday <ericfriday@gmail.com>
2017-12-16 23:58:48 -0600

    Update 2017-12-11.cs

--- 2017-12-11.cs
+++ 2017-12-11.cs
@@ -1,60 +1,49 @@
-// C# Refactor of the daily puzzle below. 
-// Question has already been solved in JS. See main repo. 
-// Currently, the various Console.WriteLines are for troubleshooting. 
-
 using System;
-using System.Diagnostics;
+// using System.Diagnostics;
 using System.Text;
-using System.Numerics;
+// using System.Numerics;
 
 public class Program
 {
-	public static int Main()
+	public static void Main()
 	{
-		int n = 1888897; // number to be calculated
+		int n = 20; // number to be calculated
 		
 		var answer = new StringBuilder();
 		
 		if (n == 0)
 		{
 			Console.WriteLine("n = 0 -> returning 1");
-			answer.Append("1");
+			answer.Append("1 ");
+			Console.WriteLine(answer);
+			return;
 		}
 		
-		string binary = Convert.ToString(n, 2); // works to convert to binary
-		
-		string[] binaryArr = binary.Split('1');
-		
-		foreach (string bin in binaryArr)
-		{
-			Console.WriteLine(bin, bin.Length);
-			if (bin.Length % 2 != 0 || bin.Length != 0)
+		for (int i = 0; i <= n; i++)
+		{	
+			if (i == 0)
 			{
-				Console.WriteLine(bin);
-				Console.WriteLine("bin.Length was odd");
-// 				return 1;
+				answer.Append("1 ");
 			}
-			else if (bin.Length % 2 == 0 && bin.Length != 0)
+			string binary = Convert.ToString(i, 2); 
+
+			string[] binaryArr = binary.Split(new char[] {'1'}, StringSplitOptions.RemoveEmptyEntries);
+			
+			foreach (string bin in binaryArr)
 			{
-				Console.WriteLine(bin);
-				Console.WriteLine("bin.Length was even");
+				if (i == 0)
+				{
+					break;
+				}
+				else if (bin.Length % 2 == 0 && i != 0)
+				{
+					answer.Append("1 ");
+					break;
+				}
+				answer.Append("0 ");
 			}
-			else if (bin.Length == 0)
-			{
-				Console.WriteLine(bin);
-				Console.WriteLine("bin.Length was 0");
-			}
-			else
-			{
-				Console.WriteLine(bin);
-				Console.WriteLine("bin.Length error");
-			}
+			Console.WriteLine(answer);
 		}
-		
-		//int[] binaryInts = new int[Convert.ToString(n, 2).Length];
-		//string[] zeroInts = new string[binary.Split("1")];
-		
 		Console.WriteLine(answer);
-		return 0;
 	}
 }
